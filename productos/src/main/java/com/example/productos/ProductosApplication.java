2608cb695325be1977cead44c3c7c40878822ee2
Jesenia1707 <jes.pardo@duocuc.cl>
2026-09-13 20:23:49 -0300

    feat: configuracion de base de datos

--- productos/src/main/java/com/example/productos/ProductosApplication.java
+++ productos/src/main/java/com/example/productos/ProductosApplication.java
@@ -1,4 +1,4 @@
-package com.pedidos360.productos;
+package com.example.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
